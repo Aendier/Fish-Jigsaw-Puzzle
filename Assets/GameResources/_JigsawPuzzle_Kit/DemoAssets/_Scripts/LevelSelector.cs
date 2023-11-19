774455f5f2b5f6d825e3d827dadbde631a75fda1
Aendier <1628843071@qq.com>
2023-11-19 18:58:29 +0800

    no message

---
+++ Assets/GameResources/_JigsawPuzzle_Kit/DemoAssets/_Scripts/LevelSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    public void Select()
+    {
+        SceneManager.LoadScene("Puzzle_Classic_Level "+ level);
+    }
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Select);
+    }
+}
---
+++ Assets/GameResources/_Scripts/LevleContorller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevleContorller : MonoBehaviour
+{
+    public GameController gameController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
